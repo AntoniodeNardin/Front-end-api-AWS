e29be4395f1e394a672fa38ffe503f5a545a7e59
GuilhermeXavier22 <guilhermexavier339@gmail.com>
2022-04-14 17:16:09 -0300

    versao inicial

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+    <header>
+
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/sale">Sale</RouterLink>
+            </nav>
+    </header>
+
+    <RouterView />
+</template>
+
+<style>
+
+</style>
---
+++ src/views/LastestSale.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <h1>hello from sale page</h1>
+
+        <div v-if="loading">Carregando Lista....</div>
+
+        <table v-else>
+
+            <thead>
+
+                <tr>
+
+                    <th>Nome do aluno</th>
+                    <th>Nome do curso</th>
+                    <th>Data</th>
+                    <th>valor</th>
+
+                </tr>
+
+            </thead>
+
+            <tbody>
+
+                <tr v-for="sale in listSale" v-bind:key="sale.id">
+                
+                    <td>{{sale.Name}}</td>
+                    <td>{{sale.Name_Product}}</td>
+                    <td>{{sale.Sale_Date}}</td>
+                    <td></td>
+                    <td>{{sale.Total}} </td> 
+                    
+                </tr> 
+
+                <button @click="getListSale()"> listar vendas</button>
+
+            </tbody> 
+
+        </table>
+
+
+    </div>
+</template>
+
+<script>
+
+    export default{
+        data(){
+            return {
+                listSale: [],
+                loading:false,
+            }
+        },
+
+        mounted(){
+            this.getListSale()
+        },
+
+        methods:{
+            getListSale(){
+                const api = 'https://dashboardmege.herokuapp.com/api/sales'
+                this.loading = true 
+                this.axios.get(api).then((response) =>{
+                    this.listSale = response.data
+                    this.loading = false
+                } )
+            }
+    
+        }
+
+
+    }
+
+</script>
+
+<style>
+
+</style>
